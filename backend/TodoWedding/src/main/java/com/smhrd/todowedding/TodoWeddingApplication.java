d50a5940c9530f100c52df31cd14067882387711
sin6338ki <jyshin38@gmail.com>
2023-09-01 12:33:50 +0900

    feat: 프로젝트 생성

---
+++ backend/TodoWedding/src/main/java/com/smhrd/todowedding/TodoWeddingApplication.java
@@ -0,0 +1,13 @@
+package com.smhrd.todowedding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoWeddingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoWeddingApplication.class, args);
+	}
+
+}
---
+++ backend/TodoWedding/src/test/java/com/smhrd/todowedding/TodoWeddingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smhrd.todowedding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoWeddingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
