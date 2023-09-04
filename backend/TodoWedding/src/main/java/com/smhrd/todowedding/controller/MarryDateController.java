78fd4faa601457776f57fa162ae7f5ffa8b17a51
sin6338ki <jyshin38@gmail.com>
2023-09-04 18:04:49 +0900

    feat: findMarryDate 기능 추가

--- backend/TodoWedding/src/main/java/com/smhrd/todowedding/controller/MarryDateController.java
+++ backend/TodoWedding/src/main/java/com/smhrd/todowedding/controller/MarryDateController.java
@@ -1,7 +1,11 @@
 package com.smhrd.todowedding.controller;
 
+import java.sql.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,7 +30,14 @@ public class MarryDateController {
 	@PostMapping(value="marrydate")
 	public int addMarryDate(@RequestBody MarryDateDto marryDateDto) {
 		
-		log.info("client and backend communication success..... : " + marryDateDto.getMarryDt() + " , " + marryDateDto.getMemberSeq());
+		log.info("addMarryDate - client and backend communication success..... : " + marryDateDto.getMarryDt() + " , " + marryDateDto.getMemberSeq());
 		return service.addMarryDate(marryDateDto);
+	}
+	
+	//memberSeq에 대한 marryDate 조회하기 
+	@GetMapping(value="marrydate/{memberSeq}")
+	public String findMarryDate(@PathVariable(name="memberSeq") int memberSeq) {
+		log.info("findMarryDate - communication success..... : " + memberSeq);
+		return service.findMarryDate(memberSeq);
 	}
 }
