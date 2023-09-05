ff936b0c97d1a2f37f0bcc6595c26b30f02ccfe6
양수진 <soojin9885@naver.com>
2023-09-05 18:02:27 +0900

    todolist 기능생성중

--- frontend/todowedding/src/components/TodoList/TodoList.jsx
+++ frontend/todowedding/src/components/TodoList/TodoList.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 
+const style = {
+   bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]`
+}
+
 const TodoList = () => {
   return (
-    <div>TodoList</div>
-  )
+    <div className={style.bg}>
+       <div className={style.container}>
+        <h3 className={style.heading}>Todo List😎</h3>
+        <form className={style.form}>
+           <input className={style.input} type="text" placeholder='Add Todo'/>
+           <button className={style.button}></button>
+        </form>
+       </div>
+    </div>
+  );
 }
 
 export default TodoList
