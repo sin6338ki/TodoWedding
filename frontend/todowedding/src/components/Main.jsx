f157502095259391a8d207e1a37c088c8fad964b
서현록 <sh0501sh11@gmail.com>
2023-09-05 18:36:55 +0900

    design: 메인 디자인 변경

--- frontend/todowedding/src/components/Main.jsx
+++ frontend/todowedding/src/components/Main.jsx
@@ -1,30 +1,40 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-import "../App.css";
+import '../App.css';
+
+import Guide from '../../src/assets/images/todo_main.png'
+
+//메인 메뉴 아이콘
+import Calendar from '../../src/assets/images/icon/calendar.png'
+import TodoList from '../../src/assets/images/icon/todolist.png'
+import Budget from '../../src/assets/images/icon/budget.png'
+import Map from '../../src/assets/images/icon/map.png'
 
 const Main = () => {
   return (
     <div>
-      <img src="/images/todo_main.png" alt="Guide" width="px"/>      
+      <img src={Guide} alt="Guide" width="px"/>
+
         <div style={{display:'flex', alignItems:'center', margin:'5%'}}>
-        <Link to="todowedding/calendar">
-          <img src="/images/icon/calendar.png" alt="Calender" width="50px"/>
-          <span>일정관리</span>
-        </Link>
-        <Link to="todowedding/todolist">
-          <img src="/images/icon/todolist.png" alt="TodoList" width="50px"/>
-          <span>투두리스트</span>
-        </Link>
+          <Link to="todowedding/calendar">
+            <img src={Calendar} alt="Calender" width="50px"/>
+            <span>일정관리</span>
+          </Link>
+          <Link to="todowedding/todolist">
+            <img src={TodoList} alt="TodoList" width="50px"/>
+            <span>투두리스트</span>
+          </Link>
         </div>
+
         <div style={{display:'flex', alignItems:'center', margin:'5%'}}>
-        <Link to="todowedding/budget">
-          <img src="/images/icon/budget.png" alt="Budget" width="50px"/>
-          <span>예산관리</span>
-        </Link>       
-        <Link to="todowedding/map">
-          <img src="/images/icon/map.png" alt="Map" width="50px"/>
-          <span>업체찾기</span>
-        </Link>
+          <Link to="todowedding/budget">
+            <img src={Budget} alt="Budget" width="50px"/>
+            <span>예산관리</span>
+          </Link>       
+          <Link to="todowedding/map">
+            <img src={Map} alt="Map" width="50px"/>
+            <span>업체찾기</span>
+          </Link>
         </div>     
     </div>
   )
