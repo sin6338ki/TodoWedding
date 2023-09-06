4fb1b1651cfc0ae9c2a6677a06a3325776d98429
서현록 <sh0501sh11@gmail.com>
2023-09-06 17:29:09 +0900

    feat: modal 추가 중

--- frontend/todowedding/src/components/Main.jsx
+++ frontend/todowedding/src/components/Main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-
-import Guide from '../../src/assets/images/todo_main.png'
+import Guide from '../../src/assets/images/todo_main.png';
 
 //메인 메뉴 아이콘
 import Calendar from '../../src/assets/images/icon/calendar_bg.png'
@@ -13,24 +12,21 @@ const Main = () => {
   return (
     <div>
       <img src={Guide} alt="Guide"/>
-        <div style={{display:'flex', alignItems:'center'}}>
-          <Link to="todowedding/calendar" className='menu-top'>
-            <img src={Calendar} alt="Calender" width="80px"/>
+      <div style={{display:'flex'}}>
+          <Link to="todowedding/calendar" className='main-menu'>
+            <img src={Calendar} alt="Calender" width="70px"/>
             <span className="menu" class='text-sm'>일정관리</span>
           </Link>
-          <Link to="todowedding/todolist" className='menu-top'>
-            <img src={TodoList} alt="TodoList" width="80px"/>
+          <Link to="todowedding/todolist" className='main-menu'>
+            <img src={TodoList} alt="TodoList" width="70px"/>
             <span className="menu" class='text-sm'>TodoList</span>
           </Link>
-        </div>
-
-        <div style={{display:'flex', alignItems:'center'}}>
-          <Link to="todowedding/budget" className='menu-bottom'>
-            <img src={Budget} alt="Budget" width="80px"/>
+          <Link to="todowedding/budget" className='main-menu'>
+            <img src={Budget} alt="Budget" width="70px"/>
             <span className="menu"  class='text-sm'>예산관리</span>
           </Link>       
-          <Link to="todowedding/map" className='menu-bottom'>
-            <img src={Map} alt="Map" width="80px"/>
+          <Link to="todowedding/map" className='main-menu'>
+            <img src={Map} alt="Map" width="70px"/>
             <span className="menu"  class='text-sm'>업체찾기</span>
           </Link>
         </div>     
--- frontend/todowedding/src/components/header/header.jsx
+++ frontend/todowedding/src/components/header/header.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-import TodoLogo from '../../assets/images/todo_logo.png';
+import TodoLogo from '../../assets/images/logo6-removebg-preview.png';
 
-const header = () => {
+const Header = () => {
   return (
     <div className='header-bar'>
         <img src={TodoLogo} alt="ToDo" width="90px" 
         onClick={()=>{window.location.href="/"}}/>
         <Link to="/todowedding/login" >
-          <span>로그인</span>
+          <span className='main-login text-sm'>로그인</span>
         </Link> 
     </div>
   )
 }
 
-export default header
+export default Header
