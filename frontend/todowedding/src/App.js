648fbb67ff588202520c727c18aa03d07f62f899
sin6338ki <jyshin38@gmail.com>
2023-09-07 18:31:29 +0900

    fix : 문제해결

--- frontend/todowedding/src/App.js
+++ frontend/todowedding/src/App.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Header from "./components/header/Header";
+import Header from "./components/Header/Header";
 import BottomBar from "./components/BottomBar/BottomBar";
 import Kakaologin from "./components/Login/Kakaologin";
 import KakaologOut from "./components/Login/KakaologOut";
