481185fce74aac6ca5ffb209e467e36496c7496c
sin6338ki <jyshin38@gmail.com>
2023-09-19 14:23:30 +0900

    fix: merge 후 에러 해결

--- frontend/todowedding/src/components/Budget/BudgetIndex.jsx
+++ frontend/todowedding/src/components/Budget/BudgetIndex.jsx
@@ -8,7 +8,6 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import Tab from "react-bootstrap/Tab";
 import Tabs from "react-bootstrap/Tabs";
-import axios from "axios";
 import { useSelector } from "react-redux";
 
 import IncomeList from "./IncomeList";
