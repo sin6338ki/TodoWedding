36b67d9312f2a9f9338472bc69d5ef5393bca14e
sin6338ki <jyshin38@gmail.com>
2023-09-20 15:28:31 +0900

    fix: merge 후 에러해결

--- frontend/todowedding/src/components/Budget/BudgetRoleChart.jsx
+++ frontend/todowedding/src/components/Budget/BudgetRoleChart.jsx
@@ -1,41 +1,37 @@
-import React from 'react'
-import {Chart as ChartJS, ArcElement, Tooltip, Legend} from 'chart.js'
-import {pie} from 'react-chartjs-2'
+import React from "react";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Pie } from "react-chartjs-2";
 
-const BudgetRoleChart = ( {brideCnt , broomCnt , bothCnt , etcCnt}) => {
+const BudgetRoleChart = ({ brideCnt, broomCnt, bothCnt, etcCnt }) => {
     ChartJS.register(ArcElement, Tooltip, Legend);
 
     const data = {
-    label : ["신부","신랑","공동","기타"],
-    datasets : [
-         {
-            labels : "expense",
-            data : [brideCnt , broomCnt , bothCnt , etcCnt] ,
-            backgroundColor: [
-        
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                
-              ],
-              borderColor: [
-               
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-              
-              ],
-              borderWidth: 0.5,
-         }
-    ],
+        label: ["신부", "신랑", "공동", "기타"],
+        datasets: [
+            {
+                labels: "expense",
+                data: [brideCnt, broomCnt, bothCnt, etcCnt],
+                backgroundColor: [
+                    "rgba(54, 162, 235, 0.2)",
+                    "rgba(255, 206, 86, 0.2)",
+                    "rgba(75, 192, 192, 0.2)",
+                    "rgba(153, 102, 255, 0.2)",
+                ],
+                borderColor: [
+                    "rgba(54, 162, 235, 1)",
+                    "rgba(255, 206, 86, 1)",
+                    "rgba(75, 192, 192, 1)",
+                    "rgba(153, 102, 255, 1)",
+                ],
+                borderWidth: 0.5,
+            },
+        ],
     };
-  return (
-    <div>
-        <Pie data = {data}/>
-    </div>
-  )
-}
+    return (
+        <div>
+            <Pie data={data} />
+        </div>
+    );
+};
 
-export default BudgetRoleChart
+export default BudgetRoleChart;
