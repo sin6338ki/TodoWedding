7813dafcd152563d2105d00c47c0a06628cf8feb
sin6338ki <jyshin38@gmail.com>
2023-09-20 09:36:58 +0900

    fix: merge 후 에러 처리

--- frontend/todowedding/src/components/AddInfo/TotalBudget.jsx
+++ frontend/todowedding/src/components/AddInfo/TotalBudget.jsx
@@ -21,59 +21,25 @@ const TotalBudget = () => {
     //초기 상태에 등록된 총예산이 있는지 나타내는 상태 변수 추가 (hasTotalBudget)
     const [hasTotalBudget, setHasTotalBudget] = useState(false);
 
-  //등록된 총 예산이 있는지 확인
-  useEffect(() => {
-    const fetchTotalBudget = async () => {
-      try {
-      //백엔드로 총 예산 조회 요청 보내기
-        const response = await axios.get(`http://localhost:8085/totalbudget/select/${userSeq}`);
-        if(response.data) {
-          setTotalBudget(response.data);
-          setHasTotalBudget(true);
-          console.log("등록된 총 예산 : ", response.data.total_budget); 
-        }
-      } catch (error) {
-          console.error("총 예산 조회 에러 : ", error);
-      }
-    };
-    fetchTotalBudget();
-  },[userSeq]);
-
- 
-  // 등록/수정 버튼 클릭시
-  const handleSubmit=async(event)=>{
-    event.preventDefault();
-  
-      try{
-          let response;
-              if(hasTotalBudget){
-                //이미 등록된 총 예산이 있는 경우 수정 API 호출하기 
-                response = await axios.post("http://localhost:8085/totalbudget/update", { 
-                  total_budget: totalBudget, 
-                  member_seq: userSeq
-                });
-              }else{
-                //등록된 총 예산이 없는 경우 등록 API 호출하기 
-                response = await axios.post("http://localhost:8085/totalbudget/insert", { 
-                  total_budget: totalBudget, 
-                  member_seq: userSeq
-                });
-              }
-        
-              if(response.status===200){
-                  alert("총 예산 비용이 성공적으로 업데이트되었습니다.")
-                  nav("/todowedding/calendar");
-              }else{
-                  alert("총 예산 비용 업데이트에 실패하였습니다.")
-              }
-              }catch(error){
-                  console.error(error)
+    //등록된 총 예산이 있는지 확인
+    useEffect(() => {
+        const fetchTotalBudget = async () => {
+            try {
+                //백엔드로 총 예산 조회 요청 보내기
+                const response = await axios.get(`http://localhost:8085/totalbudget/select/${userSeq}`);
+                if (response.data) {
+                    setTotalBudget(response.data);
+                    setHasTotalBudget(true);
+                    console.log("등록된 총 예산 : ", response.data.total_budget);
+                }
+            } catch (error) {
+                console.error("총 예산 조회 에러 : ", error);
             }
         };
         fetchTotalBudget();
     }, [userSeq]);
 
-    //폼 제출 핸들러 함수
+    // 등록/수정 버튼 클릭시
     const handleSubmit = async (event) => {
         event.preventDefault();
 
@@ -94,10 +60,10 @@ const TotalBudget = () => {
             }
 
             if (response.status === 200) {
-                alert("총 예상 비용이 성공적으로 업데이트되었습니다.");
-                nav("/");
+                alert("총 예산 비용이 성공적으로 업데이트되었습니다.");
+                nav("/todowedding/calendar");
             } else {
-                alert("총 예상 비용 업데이트에 실패하였습니다.");
+                alert("총 예산 비용 업데이트에 실패하였습니다.");
             }
         } catch (error) {
             console.error(error);
@@ -107,7 +73,7 @@ const TotalBudget = () => {
     return (
         <div>
             <form onSubmit={handleSubmit}>
-                <div className="marrydate-header pt-3">총 예산을 입력해주세요</div>
+                <div className="marrydate-header">총 예산을 입력해주세요</div>
                 <div className="marrydate-contents">
                     <p>↓ 총 예산 비용 ↓ </p>
                     <input type="number" value={totalBudget} onChange={(e) => setTotalBudget(e.target.value)} />
