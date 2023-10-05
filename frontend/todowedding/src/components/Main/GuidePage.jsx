1a67e4bd9a900c656c9e4fbbf67c7bb17b13b9ec
sin6338ki <jyshin38@gmail.com>
2023-10-05 09:40:33 +0900

    feat(FE): 반응형 변경 및 가이드 페이지 버튼 추가 - hyunrok

--- frontend/todowedding/src/components/Main/GuidePage.jsx
+++ frontend/todowedding/src/components/Main/GuidePage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 //Guide Image
 import Guide1 from "../../assets/images/Card-News/Guide1.png";
@@ -15,12 +16,21 @@ import Guide5 from "../../assets/images/Card-News/Guide5.png";
  */
 
 const GuidePage = () => {
+    const nav = useNavigate();
+
     const { id } = useParams();
     const images = [Guide1, Guide2, Guide3, Guide4, Guide5];
+
+    const toMain = () => {
+        nav("/");
+    };
 
     return (
         <div style={{ marginTop: "81px" }}>
             <img src={images[Number(id) - 1]} alt={`Image${id}`} />
+            <button onClick={toMain} className="guide-to-main">
+                메인으로 돌아가기
+            </button>
         </div>
     );
 };
